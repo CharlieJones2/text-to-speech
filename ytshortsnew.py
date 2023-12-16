6a19c9636cb93a352d0e4f5f57d33a50c84815b5
Charlie Jones <125832247+CharlieJones2@users.noreply.github.com>
2023-12-16 20:46:04 +0000

    Update ytshortsnew.py

--- ytshortsnew.py
+++ ytshortsnew.py
@@ -1,16 +1,11 @@
 import math
 import random
-import requests
-import playsound
 from moviepy.editor import AudioFileClip, concatenate_audioclips, VideoFileClip, CompositeAudioClip, TextClip, CompositeVideoClip
-from moviepy.video.fx import crop
-import whisper
 import whisper_timestamped
 from tiktokvoice import tts
-import os
 
 # creating tts audio
-text = input('paste text')
+text = input('input text: ')
 if text[-1] == '.':
     text = text[:-1]
 sentences = text.split('.')
@@ -22,15 +17,11 @@ audios = []
 for audio in audio_files:
     audios.append(AudioFileClip(audio))
 audioClips = concatenate_audioclips([audio for audio in audios])
-audioClips.write_audiofile('text.mp3')  # writing the file as 'text.mp3'
-
-# Failed attempt to get the program to recognise the file
-# with audioClips as file:
-#     file.write_audiofile('text.mp3')
+audioClips.write_audiofile('text.mp3')
 
 # getting audio and video lengths
 audiofile = AudioFileClip('text.mp3')
-soundduration = math.floor(audiofile.duration) + 1
+soundduration = math.floor(audiofile.duration)  # + 1
 videofile = VideoFileClip('resources/parkour.mp4')
 duration = math.floor(videofile.duration)
 
@@ -48,33 +39,25 @@ w, h = clip.size
 clip = clip.crop(width=480, height=720, x_center=w/2, y_center=h/2)
 clip.audio = mixed
 
-# checking text.mp3 has been created successfully
-print(os.path.exists('text.mp3'))
-
-# checking path
-current_directory = os.path.abspath(os.path.dirname(__file__))
-file_path = os.path.join(current_directory, 'text.mp3')
-print('file path:', file_path)
-
 # making subtitles
-model = whisper.load_model('base')
-audio = whisper.load_audio('text.mp3')  # FileNotFoundError: [WinError 2] The system cannot find the file specified
-results = model.transcribe(audio)
+model = whisper_timestamped.load_model('base')
+audio = whisper_timestamped.load_audio('text.mp3')
+results = whisper_timestamped.transcribe(model, audio)
 
 # adding subtitles
-subs = list()
+subs = []
 subs.append(clip)
 for segment in results['segments']:
-    for word in segment['words']:
-        text = word['text'].upper()
-        start = word['start']
-        end = word['end']
+    text = segment['text'].upper()
+    words = text.split()
+    for word in words:
+        start = segment['start']  # problem?
+        end = segment['end']  # problem?
         duration = end - start
-        txt_clip = TextClip(txt=text, fontsize=40, font='Arial-Bold', stroke_width=2, stroke_color='black', color='white')
+        txt_clip = TextClip(txt=word, fontsize=40, font='Arial-Bold', stroke_width=2, stroke_color='black', color='white')
         txt_clip = txt_clip.set_start(start).set_duration(duration).set_pos(('center', 'center'))
         subs.append(txt_clip)
 
-clip = CompositeVideoClip(subs)
-
 # outputting video
-clip.write_videofile('output.mp4')
+final_clip = CompositeVideoClip(subs)
+final_clip.write_videofile('output.mp4', fps=30, audio_codec='aac', audio_bitrate='192k')
